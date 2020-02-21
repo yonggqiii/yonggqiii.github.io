363c8b2617bc92337f4accf290636f27e8c4a3e7
yonggqiii <yongqifoo@gmail.com>
2020-02-22 05:51:20 +0800

    Basic JS

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// Do not amend these!
+function run() {
+  var input = document.getElementById("input").value.split(" ");
+  if (input.length >= 3) {
+    main(parseInt(input[0]), parseInt(input[1]), parseInt(input[2]));
+  } else {
+    console.log("Please enter 3 numbers.");
+  }
+}
+
+function main(a, b, c) {
+  // Use a, b and c to help you!
+  // TODO: Your code below.
+}
