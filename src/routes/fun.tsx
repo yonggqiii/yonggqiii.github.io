bf91dc0b4151cadc259de2bd82309a884d39f836
Foo Yong Qi <yongqifoo@gmail.com>
2023-10-26 13:19:18 +0800

    include latex templates

--- src/routes/fun.tsx
+++ src/routes/fun.tsx
@@ -1,13 +1,23 @@
 import { Title } from "solid-start";
-import { HttpStatusCode } from "solid-start/server";
+import Card from "../components/Card";
 
 export default function Fun() {
   return (
     <main>
       <Title>Fun - Foo Yong Qi</Title>
-      <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
-      <p class="red">Page coming soon...</p>
+      <h1>Fun</h1>
+      <Card border="mauve" header_color="green" header="LaTeX Templates">
+        <p>
+          Templates for typesetting a bunch of documents I frequently create can be found in my <a href="https://github.com/yonggqiii/">GitHub</a>.
+        </p>
+        <ul>
+        <li>
+          Exam question booklets, answer booklets, solutions manuals and assignments (questions and solutions): <a href="https://github.com/yonggqiii/teaching_templates">Teaching Templates</a>
+        </li>
+        </ul>
+    
+      </Card>
     </main>
+
   );
 }
