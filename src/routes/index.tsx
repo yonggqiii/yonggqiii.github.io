379553173ba758e91d584fdbe8b396e8e3e89781
Foo Yong Qi <yongqifoo@gmail.com>
2024-10-07 18:03:36 +0800

    news

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -29,31 +29,34 @@ export default function Home() {
         <h2>News</h2>
         <h3>2024</h3>
         <MyTable>
-        <NewsItem date={Date.parse('04 Aug 2024 00:00:00 GMT+8')}>
+        <NewsItem date={Date.parse('30 Sep 2024 23:59:00 GMT+8')}>
+          [NUS] IT5100A&mdash;Industry Readiness: Typed Functional Programming has started! Course notes are available <a href="https://yongqi.foo/it5100a-notes">here</a>.
+        </NewsItem>
+        <NewsItem date={Date.parse('04 Aug 2024 23:59:00 GMT+8')}>
           [NUS] IT5000&mdash;Introduction to Computing Foundations starts tomorrow. I will be teaching the Pure Mathematics and Discrete Structures portion (Day 1, 5 Aug). See you there!
         </NewsItem>
-        <NewsItem date={Date.parse('15 Jul 2024 00:00:00 GMT+8')}>
+        <NewsItem date={Date.parse('15 Jul 2024 23:59:00 GMT+8')}>
           I will be trialing <em>Motivating Mathematics Study in Prospective Computer Scientists with Interactive Theorem Proving</em> during IT5000&mdash;Introduction to Computing Foundations.
         </NewsItem>
-        <NewsItem date={Date.parse('01 Jul 2024 00:00:00 GMT+8')}>
+        <NewsItem date={Date.parse('01 Jul 2024 23:59:00 GMT+8')}>
           [NUS] SoC Postgraduate department has launched a new initiative for prospective M.Comp. (General Track) and M.Sc. Digital Financial Technology students to be able to replace IT5001 with another course based on their performance in a proficiency test. More information can be found in the email you received. Email it5001pt@comp.nus.edu.sg for queries.
         </NewsItem>
-        <NewsItem date={Date.parse('18 Jun 2024 00:00:00 GMT+8')}>
+        <NewsItem date={Date.parse('18 Jun 2024 23:59:00 GMT+8')}>
           [NUS] Teaching Assistantship positions for IT5100A have been filled. IT5100B and IT5100E are still recruiting.
         </NewsItem>
-        <NewsItem date={Date.parse('12 Jun 2024 00:00:00 GMT+8')}>
+        <NewsItem date={Date.parse('12 Jun 2024 23:59:00 GMT+8')}>
           [NUS] I will be teaching Pure Mathematics and Discrete Mathematics at the upcoming SoC Graduate Bootcamp held in early August. Graduate Certificate in Computing Foundations, M.Comp. (General Track) (among some others) students may look forward to this bootcamp in early August 2024.
         </NewsItem>
-        <NewsItem date={Date.parse('01 Jun 2024 00:00:00 GMT+8')}>
+        <NewsItem date={Date.parse('01 Jun 2024 23:59:00 GMT+8')}>
           [NUS] Applications for Teaching Assistantship positions for IT5100A, IT5100B and IT5100E are now open. Undergraduate and Postgraduate applicants are welcome. Please email your CV to me if interested.
 </NewsItem>
-        <NewsItem date={Date.parse('01 May 2024 00:00:00 GMT+8')}>
+        <NewsItem date={Date.parse('01 May 2024 23:59:00 GMT+8')}>
           [NUS] I will be teaching IT5100A in AY24/25 Semester 1, and IT5100B & IT5100E in AY24/25 Semester 2. M.Comp. (General Track) students can email me if interested in being a guest student in these courses.
 </NewsItem>
         </MyTable>
         <h3>2023</h3>
         <MyTable>
-        <NewsItem date={Date.parse('12 Dec 2023 00:00:00 GMT+8')}>
+        <NewsItem date={Date.parse('12 Dec 2023 23:59:00 GMT+8')}>
           [NUS] SoC Postgraduate department has launched a new initiative&mdash;the SoC Graduate Bootcamp&mdash;to help prospective students with mathematical foundations they may not have learnt during their undergraduate studies. I will be teaching the Pure Mathematics and Set Theory portions this upcoming session. Graduate Certificate in Computing Foundations, M.Comp. (General Track) (among some others) students may look forward to this bootcamp in early January 2024.
         </NewsItem>
         <NewsItem date={Date.parse('27 Nov 2023 00:00:00 GMT+8')}>
